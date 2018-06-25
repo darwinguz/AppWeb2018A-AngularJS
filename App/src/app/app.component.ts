dafe38a04342a69093ab31a9e6f91f476829e92b
Darwin Guzmán <dga_guz@hotmail.es>
2018-06-25 11:01:48 -0500

    Introduccion a Angular

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css'
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  // Interpolacion
+  titulo = 'Web';
+  usuario = {
+    nombre: 'Adrian',
+    apellido: 'Eguez'
+  };
+  arregloNumeros = [5, 4, 3, 2, 1];
+  mostrarTitulo = true;
+
+  imagenUrl = "https://coryrylan.com/assets/images/cory-rylan-gde.png";
+  imagenWidth = 200;
+
+  html = `
+  <h1>HOLA</h1>
+  <p>Hola mundo</p>
+  <img src="${this.imagenUrl}" alt="">
+`;
+
+  aumentarWidth(evento) {
+    console.log('evento', evento);
+    this.imagenWidth += 10;
+  }
+
+  restarWidth() {
+    this.imagenWidth -= 10;
+  }
 }
